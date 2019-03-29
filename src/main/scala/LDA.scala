f8319154ca46b4e56bb1e05478c1deef40dc9312
joebluems <joeblue@Josephs-MacBook-Pro.local>
2019-03-28 17:32:52 -0700

    initial load of files

---
+++ src/main/scala/LDA.scala
@@ -0,0 +1,75 @@
+package com.mapr.topics 
+
+import scala.collection.mutable
+import org.apache.spark.ml.{Pipeline, PipelineModel}
+import org.apache.spark.ml.clustering.{LDA, LDAModel}
+import org.apache.spark.ml.feature._
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+
+object Main extends App { 
+  Logger.getLogger("org").setLevel(Level.WARN)
+  var corpusFile = "./documents"
+  val vocabSize = 1000 // total vocabulary size
+  var k = 10  // number of topics
+  val iter = 10 // iterations
+    
+  if (args.length == 2) {
+      corpusFile = args(0)
+      k = args(1).toInt
+  }
+
+  val spark = SparkSession.builder.master("local").appName("example").getOrCreate()
+  import spark.implicits._
+
+  def buildDataPrepPipeline(dataset: DataFrame, vocabSize: Int): (PipelineModel, Array[String]) = {
+    val countTokens = udf { (words: Seq[String]) => words.length }
+    val stop = StopWordsRemover.loadDefaultStopWords("english") ++
+        Array("tr", "td", "div", "class", "table", "html", "div")
+    val tokenizer = new RegexTokenizer().setInputCol("doc").setOutputCol("rawwords")
+        .setGaps(false).setPattern("[a-zA-Z]{3,}")
+    val stopremover = new StopWordsRemover().setInputCol("rawwords")
+        .setOutputCol("words").setStopWords(stop)
+    val vectorizer = new CountVectorizer().setInputCol("words").setOutputCol("features")
+        .setVocabSize(vocabSize).setMinDF(2)
+    val stages = Array( tokenizer, stopremover, vectorizer)
+    val pipeline = new Pipeline().setStages(stages)
+    val model = pipeline.fit(dataset)
+    (model, model.stages(2).asInstanceOf[CountVectorizerModel].vocabulary)
+  }
+
+  def buildModel(dataset: DataFrame, k: Int, maxIter: Int): LDAModel = {
+    val lda = new LDA().
+        setK(k).setFeaturesCol("features").setMaxIter(maxIter).setOptimizer("online")
+    lda.fit(dataset)
+  }
+
+  def showTopics(spark: SparkSession, vocab: Array[String], ldaModel: LDAModel): Unit = {
+    import spark.implicits._
+    val bc = spark.sparkContext.broadcast(vocab)
+    val topicWords = udf { (indices: mutable.WrappedArray[_]) =>
+      indices.map { case v: Int => bc.value(v) }
+    }
+    ldaModel.describeTopics().select(topicWords($"termIndices").as("topics")).show(false)
+  }
+
+  /// load the corpus of documents and split into train/test
+  val dataset = spark.sparkContext.wholeTextFiles(corpusFile).toDF("filename","doc")
+  val splits = dataset.randomSplit(Array(0.8, 0.2), 15L)
+  val (train, test) = (splits(0), splits(1))
+
+  /// prepare the data and train the LDA model
+  val (dataprep, vocab) = buildDataPrepPipeline(train, vocabSize)
+  val vectorized = dataprep.transform(train)
+  val ldaModel = buildModel(vectorized, k, iter)
+
+  /// print out the topics and perplexity on test data
+  showTopics(spark, vocab, ldaModel)
+  val testVect = dataprep.transform(test)
+  val perplexity = ldaModel.logPerplexity(ldaModel.transform(testVect))
+  println(s"Perplexity=$perplexity")
+
+}
